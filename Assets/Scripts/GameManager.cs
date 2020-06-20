a982e83a46f643a81b854e91c1fa001a087da127
oczerepanow <oleksii.czerepanow@rsoftstudio.pl>
2020-06-20 16:08:44 +0200

    First level canvas First level finish Player level

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+
+	bool gameHasEnded = false;
+
+	public float restartDelay = 1f;
+	public GameObject completeLevelUI;
+	public Text levelText;
+	public PlayerMovement movement;
+
+	private int playerLevel = 1;
+
+	public static int currentResolutionIndex = 0;
+
+	void Start()
+	{
+		levelText.text = "Player Level: " + playerLevel.ToString();
+
+	}
+
+	void addLevel()
+	{
+		playerLevel = playerLevel + 1;
+	}
+
+	public int getPlayerLevel()
+	{
+		return this.playerLevel;
+	}
+
+	public void setPlayerLevel(int playerLevel)
+	{
+		this.playerLevel = playerLevel;
+	}
+
+
+	public void CompleteLevel()
+	{
+		movement.enabled = false;
+		addLevel();
+		completeLevelUI.SetActive(true);
+	}
+
+	public void EndGame()
+	{
+		if (gameHasEnded == false)
+		{
+			gameHasEnded = true;
+			Invoke("Restart", restartDelay);
+		}
+
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
